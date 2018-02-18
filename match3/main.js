7b65a9f22feec8a74a9a977b278f7ee80db40c50
Zack <zseliger@gmail.com>
2018-02-18 13:44:49 -0600

    new client in match3 :)

--- match3/main.js
+++ match3/main.js
@@ -3,6 +3,8 @@ var GAME_WIDTH = 1920;
 var GAME_HEIGHT = 1080;
 var TILE_SPACING = 200;
 var TILE_SIZE = 140;
+var ROWS = 8;
+var COLS = 8;
 FRAME.loadImage("assets/1.png", "tile1");
 FRAME.loadImage("assets/2.png", "tile2");
 FRAME.loadImage("assets/3.png", "tile3");
@@ -11,6 +13,19 @@ FRAME.loadImage("assets/5.png", "tile5");
 FRAME.loadImage("assets/6.png", "tile6");
 FRAME.loadImage("assets/7.png", "tile7");
 FRAME.loadImage("assets/grid.png", "grid");
+
+function getXIndexValue(x) {
+	return (Math.round(x)/TILE_SPACING + (ROWS-1)/2);
+}
+function getYIndexValue(y) {
+	return (Math.round(y)/TILE_SPACING + (COLS-1)/2);
+}
+function getXCoordValue(x) {
+	return (x*TILE_SPACING-(TILE_SPACING/2*(ROWS-1)));
+}
+function getYCoordValue(y) {
+	return (y*TILE_SPACING-(TILE_SPACING/2*(COLS-1)));
+}
 
 class EmptyVisual extends GameObj {
 	constructor() {
@@ -26,9 +41,15 @@ class EmptyVisual extends GameObj {
 	render() {}
 }
 
+class Leaderboard extends Actor {
+	constructor() {
+		super();
+	}
+}
+
 class Tile extends Actor {
 	constructor(x,y,type=-1) {
-		super(x, y);
+		super(x, -GAME_HEIGHT*2);
 		this.alpha = 1.0;
 		this.type = type;
 		this.moused = false;
@@ -39,8 +60,8 @@ class Tile extends Actor {
 		this.height = this.normalHeight;
 		this.xOffset = 0;
 		this.yOffset = 0;
-		this.targetX = x;
-		this.targetY = y;
+		this.targetX = getXIndexValue(x);
+		this.targetY = getYIndexValue(y);
 	}
 	update() {
 		if (checkCollision(mouse, this)) {
@@ -53,11 +74,15 @@ class Tile extends Actor {
 			this.width += (this.normalWidth - this.width) * 0.1;
 			this.height += (this.normalHeight - this.height) * 0.1;
 		}
-		this.x += (this.targetX - this.x) * 0.1;
-		this.y += (this.targetY - this.y) * 0.1;
+		this.x += (getXCoordValue(this.targetX) - this.x) * 0.1;
+		this.y += (getYCoordValue(this.targetY) - this.y) * 0.1;
 	}
 	render() {
-		if (this.type < 0) return;
+		if (this.type < 0) {
+			this.ctx.fillStyle = "#37B";
+			this.ctx.fillRect(-this.width/2, -this.height/2, this.width, this.height);
+			return;
+		}
 		this.ctx.globalAlpha = this.alpha;
 		this.ctx.drawImage(this.image, -this.width/2+this.xOffset, -this.height/2+this.yOffset, this.width, this.height);
 		this.ctx.globalAlpha = 1.0;
@@ -71,6 +96,7 @@ class Tile extends Actor {
 class Grid extends Actor {
 	constructor(rows, cols) {
 		super();
+		this.score = 0;
 		this.rows = rows;
 		this.cols = cols;
 		this.selectedTile = null;
@@ -89,6 +115,19 @@ class Grid extends Actor {
 		for (var x = 0; x < this.cols; x++) {
 			for (var y = 0; y < this.rows; y++) {
 				this.tiles[x][y].update();
+				if (this.tiles[x][y].type == -1) {
+					var respawnType = this.tiles[x][y].respawnType;
+					this.tiles[x].splice(y, 1);
+					this.tiles[x].unshift(new Tile(getXCoordValue(x),0,respawnType));
+					this.tiles[x][0].targetX = x;
+					this.tiles[x][0].targetY = 0;
+					for (var x2 = 0; x2 < this.cols; x2++) {
+						for (var y2 = 0; y2 < this.rows; y2++) {
+							this.tiles[x2][y2].targetX = x2;
+							this.tiles[x2][y2].targetY = y2;
+						}
+					}
+				}
 				if (this.selectedTile == null && this.tiles[x][y].moused == true && mouse.clicking == true) {
 					this.selectedTile = this.tiles[x][y];
 				}
@@ -98,8 +137,8 @@ class Grid extends Actor {
 		//swap selected tile
 		if (mouse.clicking == false && this.selectedTile != null) {
 			if (Math.abs(this.selectedTile.xOffset) + Math.abs(this.selectedTile.yOffset) > TILE_SPACING/2) {
-				var xIndex = this.getXIndexValue(this.selectedTile);
-				var yIndex = this.getYIndexValue(this.selectedTile);
+				var xIndex = getXIndexValue(this.selectedTile.x);
+				var yIndex = getYIndexValue(this.selectedTile.y);
 				var targetX = xIndex;
 				var targetY = yIndex;
 				if (this.selectedTile.xOffset > 0) targetX = xIndex + 1;
@@ -138,27 +177,33 @@ class Grid extends Actor {
 		}
 		if (this.selectedTile != null) this.selectedTile.draw();
 	}
-	changeTiles(tiles) {
-		for (var x = 0; x < this.cols; x++) {
-			for (var y = 0; y < this.rows; y++) {
-				if (this.tiles[x][y].type != tiles[x][y].type) {
-					var xVal = this.tiles[x][y].x;
-					var yVal = this.tiles[x][y].y;
-					this.addTile(x, y, tiles[x][y].type);
-					this.tiles[x][y].targetX = xVal;
-					this.tiles[x][y].targetY = yVal;
-				}
-			}
+	addTiles(tiles) {
+		for (var i = 0; i < tiles.length; i++) {
+			this.addTile(tiles[i].x, tiles[i].y, tiles[i].type);
+			this.tiles[tiles[i].x][tiles[i].y].respawnType = tiles[i].respawnType;
 		}
 	}
 	addTile(x, y, type) {
-		this.tiles[x][y] = new Tile(x*TILE_SPACING-(TILE_SPACING/2*(this.rows-1)), y*TILE_SPACING-(TILE_SPACING/2*(this.cols-1)), type);
+		this.tiles[x][y] = new Tile(getXCoordValue(x), getYCoordValue(y), type);
 	}
-	getXIndexValue(tile) {
-		return (Math.round(tile.x)/TILE_SPACING + (this.rows-1)/2);
+	swapTiles(tile1, tile2) {
+		this.tiles[tile1.x][tile1.y].targetX = tile2.x;
+		this.tiles[tile1.x][tile1.y].targetY = tile2.y;
+		this.tiles[tile2.x][tile2.y].targetX = tile1.x;
+		this.tiles[tile2.x][tile2.y].targetY = tile1.y;
+		var tiles = this.getTilesArray();
+		for (var i = 0; i < tiles.length; i++) {
+			this.tiles[tiles[i].targetX][tiles[i].targetY] = tiles[i];
+		}
 	}
-	getYIndexValue(tile) {
-		return (Math.round(tile.y)/TILE_SPACING + (this.cols-1)/2);
+	getTilesArray() {
+		var arr = [];
+		for (var x = 0; x < this.cols; x++) {
+			for (var y = 0; y < this.rows; y++) {
+				arr.push(this.tiles[x][y]);
+			}
+		}
+		return arr;
 	}
 }
 
@@ -170,14 +215,28 @@ window.onload = function() {
 	keyboard = new Keyboard();
 	mouse = new Mouse();
 	
+	scoreText = new Text(-GAME_WIDTH/2+175,GAME_HEIGHT-225,"Score: 0","Arial","#222",50);
 	var gridActor = new ImageActor(0,0,FRAME.getImage("grid"),3.13);
 	mainCollection = new Collection();
+	mainCollection.add(scoreText);
 	mainCollection.add(gridActor);
 	
 	//types of packets
-	network.addPacketType("updateTiles", function(packet) {
+	network.addPacketType("addTiles", function(packet) {
 		var obj = network.getObj(packet.id);
-		obj.grid.changeTiles(packet.tiles);
+		obj.grid.addTiles(packet.tiles);
+	});
+	network.addPacketType("moveTiles", function(packet) {
+		
+	});
+	network.addPacketType("swapTiles", function(packet) {
+		var obj = network.getObj(packet.id);
+		obj.grid.swapTiles(packet.tile1, packet.tile2);
+	});
+	network.addPacketType("changeScore", function(packet) {
+		var obj = network.getObj(packet.id);
+		obj.grid.score = packet.score;
+		scoreText.text = "Score: " + obj.grid.score;
 	});
 	
 	//types of objects
@@ -186,16 +245,13 @@ window.onload = function() {
 		function(obj, packet) {
 			obj.visual = new EmptyVisual();
 			obj.grid = new Grid(packet.grid.rows, packet.grid.cols);
-			obj.grid.changeTiles(packet.grid.tiles);
+			obj.grid.addTiles(packet.tiles);
 			mainCollection.add(obj.grid);
 		}, undefined, undefined,
 		function (obj, packet) {
 			mainCollection.remove(obj.grid);
 		}
 	);
-	
-	network.createSocket("ws://match-3---.herokuapp.com");
-	main();
 }
 
 function main() {
@@ -216,6 +272,16 @@ function main() {
 	
 	network.update();
 	requestFrame(main);
+}
+
+function startGame() {
+	document.getElementById("preGameGUI").style.visibility = "hidden";
+	network.createSocket("wss://match-3---.herokuapp.com");
+	/*network.currentPackets.push({
+		type: "playerJoin",
+		name: document.getElementById("name").value
+	});*/
+	main();
 }
 
 function checkCollision(obj1, obj2) {
