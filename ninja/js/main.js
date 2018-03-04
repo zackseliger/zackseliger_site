311e0fa9f23ad483f2f4ff49525917e87ebb0997
Zack <zseliger@gmail.com>
2018-03-04 08:58:54 -0600

    ninja update

--- ninja/js/main.js
+++ ninja/js/main.js
@@ -33,16 +33,16 @@ class WhiteBack extends Actor {
 		super(ARENA_X, ARENA_Y);
 	}
 	update() {
-		this.x = -FRAME.x;
-		this.y = -FRAME.y;
+		this.x = -FRAME.x/FRAME.scaleX;
+		this.y = -FRAME.y/FRAME.scaleY;
 		if (this.x < 0) this.x = 0;
-		else if (this.x + window.innerWidth > ARENA_WIDTH) this.x = ARENA_WIDTH - window.innerWidth;
+		else if (this.x + window.innerWidth/FRAME.scaleX > ARENA_WIDTH) this.x = ARENA_WIDTH - window.innerWidth/FRAME.scaleX;
 		if (this.y < 0) this.y = 0;
-		else if (this.y + window.innerHeight > ARENA_HEIGHT) this.y = ARENA_HEIGHT - window.innerHeight;
+		else if (this.y + window.innerHeight/FRAME.scaleY > ARENA_HEIGHT) this.y = ARENA_HEIGHT - window.innerHeight/FRAME.scaleY;
 	}
 	render() {
 		this.ctx.fillStyle = "#F6F6F6";
-		this.ctx.fillRect(0,0,window.innerWidth,window.innerHeight);
+		this.ctx.fillRect(0,0,window.innerWidth/FRAME.scaleX,window.innerHeight/FRAME.scaleY);
 	}
 }
 
@@ -55,10 +55,10 @@ class Grid extends Actor {
 		this.offsetY = 0;
 	}
 	update() {
-		this.x = -FRAME.x;
-		this.y = -FRAME.y;
-		this.offsetX -= this.prevFrameX - FRAME.x;
-		this.offsetY -= this.prevFrameY - FRAME.y;
+		this.x = -FRAME.x/FRAME.scaleX;
+		this.y = -FRAME.y/FRAME.scaleY;
+		this.offsetX -= this.prevFrameX - FRAME.x/FRAME.scaleX;
+		this.offsetY -= this.prevFrameY - FRAME.y/FRAME.scaleY;
 		while (this.offsetX > GRID_SPACES) this.offsetX -= GRID_SPACES;
 		while (this.offsetX < -GRID_SPACES) this.offsetX += GRID_SPACES;
 		while (this.offsetY > GRID_SPACES) this.offsetY -= GRID_SPACES;
@@ -66,22 +66,45 @@ class Grid extends Actor {
 		this.x += this.offsetX;
 		this.y += this.offsetY;
 		
-		this.prevFrameX = FRAME.x;
-		this.prevFrameY = FRAME.y;
+		this.prevFrameX = FRAME.x/FRAME.scaleX;
+		this.prevFrameY = FRAME.y/FRAME.scaleY;
 	}
 	render() {
 		this.ctx.strokeStyle = "#CCC";
 		this.ctx.lineWidth = 2;
 		this.ctx.beginPath();
-		for (var x = 0; x <= window.innerWidth; x += GRID_SPACES) {
+		for (var x = 0; x <= (window.innerWidth+GRID_SPACES)/FRAME.scaleX; x += GRID_SPACES) {
 			this.ctx.moveTo(x, -GRID_SPACES);
-			this.ctx.lineTo(x, window.innerHeight+GRID_SPACES);
+			this.ctx.lineTo(x, (window.innerHeight+GRID_SPACES)/FRAME.scaleX);
 		}
-		for (var y = 0; y <= window.innerHeight; y += GRID_SPACES) {
+		for (var y = 0; y <= (window.innerHeight+GRID_SPACES)/FRAME.scaleY; y += GRID_SPACES) {
 			this.ctx.moveTo(-GRID_SPACES, y);
-			this.ctx.lineTo(window.innerWidth+GRID_SPACES, y);
+			this.ctx.lineTo((window.innerWidth+GRID_SPACES)/FRAME.scaleY, y);
 		}
 		this.ctx.stroke();
+	}
+}
+
+class TrailParticle extends Actor {
+	constructor(obj, fadeRate) {
+		super(obj.x, obj.y, obj.rotation);
+		this.image = obj.image;
+		this.width = obj.width;
+		this.height = obj.height;
+		this.alpha = 1.0;
+		this.fadeRate = fadeRate || 0.3;
+	}
+	update() {
+		this.alpha -= this.fadeRate;
+		if (this.alpha <= 0.0) {
+			this.alpha = 0.0;
+			this.dead = true;
+		}
+	}
+	render() {
+		this.ctx.globalAlpha = this.alpha;
+		this.ctx.drawImage(this.image, -this.width/2, -this.height/2, this.width, this.height);
+		this.ctx.globalAlpha = 1.0;
 	}
 }
 
@@ -91,17 +114,20 @@ class Ninja extends Actor {
 		this.visual = owner.visual;
 		this.image = FRAME.getImage("white");
 		this.name = owner.name;
-		this.nameText = new Text(0, -175, this.name+" (0)", "Arial", "#000", 42, "center");
+		this.nameText = new Text(0, -175, this.name+" (0/5)", "Arial", "#222", 42, "center");
 		this.width = 100;
 		this.height = 100;
 		this.health = 100;
+		this.targetHealth = 100;
 		this.numStars = 0;
 		this.clicking = false;
 	}
 	update() {
 		this.x = this.visual.position.x;
 		this.y = this.visual.position.y;
-		this.nameText.text = this.name+" ("+this.numStars+")";
+		this.nameText.text = this.name+" ("+this.numStars+"/5)";
+		this.health += (this.targetHealth - this.health) * 0.1;
+		addTrail(this);
 	}
 	render() {
 		this.ctx.drawImage(this.image, -this.width/2, -this.height/2, this.width, this.height);
@@ -116,29 +142,55 @@ class Ninja extends Actor {
 }
 
 class Star extends Actor {
-	constructor(visual) {
+	constructor(visual, id) {
 		super();
 		this.visual = visual;
 		this.image = FRAME.getImage("star");
 		this.width = 50;
 		this.height = 50;
+		this.ownerID = id;
 	}
 	update() {
 		this.x = this.visual.position.x;
 		this.y = this.visual.position.y;
 		this.rotation += 0.01;
+		if (this.ownerID != -1) {
+			addTrail(this, 0.15);
+			this.rotation += 0.02;
+		}
 	}
 	render() {
 		this.ctx.drawImage(this.image, -this.width/2, -this.height/2, this.width, this.height);
 	}
 }
 
+function addTrail(obj, rate) {
+	particleCollection.add(new TrailParticle(obj, rate));
+}
+
+window.onblur = function() {
+	keyboard[87] = false;
+	keyboard[38] = false;
+	keyboard[83] = false;
+	keyboard[40] = false;
+	keyboard[65] = false;
+	keyboard[37] = false;
+	keyboard[68] = false;
+	keyboard[39] = false;
+	mouse.clicking = false;
+}
+
 window.onload = function() {
 	network = new gameIO();
 	FRAME.init(GAME_WIDTH, GAME_HEIGHT, document.getElementById("canvas"));
+	document.getElementById("canvas").style.backgroundColor = "#EEE";
+	network.createSocket("wss://ninjaa-io.herokuapp.com");
+	//network.createSocket("ws://localhost:5000");
 	keyboard = new Keyboard();
 	mouse = new Mouse();
+	scoreText = new Text(0,0,"","Arial","#222",32);
 	backgroundCollection = new Collection();
+	particleCollection = new Collection();
 	starCollection = new Collection();
 	playerCollection = new Collection();
 	
@@ -165,7 +217,6 @@ window.onload = function() {
 		}, 
 		function (obj, packet) {
 			obj.ninja.numStars = packet.numStars;
-			obj.ninja.health = packet.health;
 		},
 		function (obj) {
 			playerCollection.remove(obj.ninja);
@@ -176,7 +227,7 @@ window.onload = function() {
 		"star",
 		function( obj, packet ) {
 			obj.visual = new EmptyVisual();
-			obj.star = new Star(obj.visual);
+			obj.star = new Star(obj.visual, packet.ownerID);
 			starCollection.add(obj.star);
 		}, function (obj) {
 			
@@ -187,30 +238,35 @@ window.onload = function() {
 	);
 	
 	network.addPacketType(
-    "playerDie",
-    function(packet) {
-        endGame();
-    }
-);
+		"playerDie",
+		function(packet) {
+			endGame();
+		}
+	);
+	
+	network.addPacketType(
+		"changeHealth",
+		function(packet) {
+			console.log(packet.id + ", " + packet.health);
+			for (var i = 0; i < network.objects.length; i++) {
+				if (network.objects[i].id == packet.id) {
+					network.objects[i].ninja.targetHealth = packet.health;
+				}
+			}
+		}
+	);
 }
 
 function endGame() {
-	backgroundCollection.clear();
-	starCollection.clear();
-	playerCollection.clear();
 	gamePlaying = false;
 	
 	document.getElementById("preGameGUI").style.visibility = "visible";
-	document.getElementById("canvas").style.visibility = "hidden";
 }
 
 function startGame() {
 	backgroundCollection.add(new WhiteBack());
 	backgroundCollection.add(new Grid());
 	document.getElementById("preGameGUI").style.visibility = "hidden";
-	document.getElementById("canvas").style.visibility = "visible";
-	network.createSocket("wss://ninjaa-io.herokuapp.com");
-	//network.createSocket("ws://localhost:5000");
 	network.currentPackets.push({
 		type: "playerJoin",
 		name: document.getElementById("name").value
@@ -225,7 +281,6 @@ function preMain() {
 		requestFrame(preMain);
 	}
 	else {
-		document.getElementById("canvas").style.backgroundColor = "#EEE";
 		FRAME.x = (-network.me.visual.position.x)*FRAME.scaleX + window.innerWidth/2;
 		FRAME.y = (-network.me.visual.position.y)*FRAME.scaleY + window.innerHeight/2;
 		gamePlaying = true;
@@ -237,50 +292,52 @@ function main() {
 	FRAME.clearScreen();
 	mouse.update();
 	
-	//camera
-	FRAME.x += ((-network.me.visual.position.x)*FRAME.scaleX + window.innerWidth/2 - FRAME.x) * 0.2;
-	FRAME.y += ((-network.me.visual.position.y)*FRAME.scaleY + window.innerHeight/2 - FRAME.y) * 0.2;
-	
 	backgroundCollection.update();
+	particleCollection.update();
 	starCollection.update();
 	playerCollection.update();
 	
 	backgroundCollection.draw();
+	particleCollection.draw();
 	starCollection.draw();
 	playerCollection.draw();
 	
+	//camera
+	FRAME.x = (-network.me.visual.position.x)*FRAME.scaleX + window.innerWidth/2;
+	FRAME.y = (-network.me.visual.position.y)*FRAME.scaleY + window.innerHeight/2;
+	
 	//input
-	var prevControls = JSON.stringify(controls);
-	if (keyboard[87] || keyboard[38]) {
-		controls.up = true;
-	}
-	else {
-		controls.up = false;
-	}
-	if (keyboard[83] || keyboard[40]) {
-		controls.down = true;
-	}
-	else {
-		controls.down = false;
-	}
-	if (keyboard[65] || keyboard[37]) {
-		controls.left = true;
-	}
-	else {
-		controls.left = false;
-	}
-	if (keyboard[68] || keyboard[39]) {
-		controls.right = true;
-	}
-	else {
-		controls.right = false;
-	}
-	if (JSON.stringify(controls) != prevControls) {
-		network.currentPackets.push({type: "updateControls", ctrls: controls});
+	if (gamePlaying == true) {
+		var prevControls = JSON.stringify(controls);
+		if (keyboard[87] || keyboard[38]) {
+			controls.up = true;
+		}
+		else {
+			controls.up = false;
+		}
+		if (keyboard[83] || keyboard[40]) {
+			controls.down = true;
+		}
+		else {
+			controls.down = false;
+		}
+		if (keyboard[65] || keyboard[37]) {
+			controls.left = true;
+		}
+		else {
+			controls.left = false;
+		}
+		if (keyboard[68] || keyboard[39]) {
+			controls.right = true;
+		}
+		else {
+			controls.right = false;
+		}
+		if (JSON.stringify(controls) != prevControls) {
+			network.currentPackets.push({type: "updateControls", ctrls: controls});
+		}
 	}
 	
 	network.update();
-	if (gamePlaying == true) {
-		requestFrame(main);
-	}
+	requestFrame(main);
 }
